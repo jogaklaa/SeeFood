3104df4df66802648e5bf10b248d0d954508cc7d
John Klaassen <jogaklaa@gmail.com>
2017-11-02 16:11:46 -0400

    Image model made

--- SeeFood/SeeAPI/models.py
+++ SeeFood/SeeAPI/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class image(models.Model):
+    positiveCertainty = models.DecimalField(decimal_places=20,max_digits=20, null = True)
+    negativeCertainty = models.DecimalField(decimal_places=20,max_digits=20, null = True)
+    photo = models.ImageField(upload_to='photos', null = True)
