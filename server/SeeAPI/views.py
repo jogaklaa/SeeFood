27df5705f9d1580a2feb9d6f7f38a40139f26cc3
John Klaassen <jogaklaa@gmail.com>
2017-11-02 17:33:51 -0400

    View work

--- server/SeeAPI/views.py
+++ server/SeeAPI/views.py
@@ -4,12 +4,23 @@ from __future__ import unicode_literals
 from django.shortcuts import render, render_to_response, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from .models import *
+import Image
 
 
 def homepageView(request):
     return render (request, "index.html")
 
 def photoCheck(request):
+    image.objects.create(photo = request.FILES['file'])
+    im = Image.open(request.FILES['file'])
+    size = [200,200]
+    im_resized = im.resize(size, Image.ANTIALIAS)
+    # TODO:fix this so that it will be acutal resolution and file names and stuff
+    im_resized.save('SOMETHINGTODOWITHTHEPHOTOHERE.png', "PNG")
+
+    #need to check validity here
+    #need to send to api here
+    #need to send to database
     return True
 
 def photoQeury(request, shortcode = None):
