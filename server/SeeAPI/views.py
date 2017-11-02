dc6af343c88102cefebd474e19f74d5cc5af0f9f
John Klaassen <jogaklaa@gmail.com>
2017-11-02 16:57:50 -0400

    We have homepage

--- server/SeeAPI/views.py
+++ server/SeeAPI/views.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 
 # Create your views here.
+def homepageView(request):
+    return render (request, "index.html")
