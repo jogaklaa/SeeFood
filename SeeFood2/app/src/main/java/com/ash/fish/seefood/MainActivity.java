60a2604ed0e33391a99cde5805c16294aa55bf2f
Unknown <ash.8@wright.edu>
2017-11-14 16:15:17 -0500

    Two images & Two Buttons
    Two images and two buttons.  They don't do anything yet, but we're preparing to send images to server.

--- SeeFood2/app/src/main/java/com/ash/fish/seefood/MainActivity.java
+++ SeeFood2/app/src/main/java/com/ash/fish/seefood/MainActivity.java
@@ -4,16 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     int i = 0;
     Button myButton;
+    ImageButton foodButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
         myButton  = (Button)findViewById(R.id.colorBtn);
         myButton.setOnClickListener(new View.OnClickListener() {
@@ -23,10 +24,17 @@ public class MainActivity extends AppCompatActivity {
                 if(i < words.length){
                     myButton.setText(words[i]);
                     i++;
-                }
+                }//end if
+            }//end onClick
+        });
 
-
+        foodButton=(ImageButton)findViewById(R.id.imageButton);
+        foodButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Hello!!!!!!!!!");
             }
         });
-    }
+    }//end onCreate
+
+
 }
