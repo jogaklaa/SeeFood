30bfce5d99c0e2c6324a74e56d2096baf4afb486
Unknown <dobson.11@wright.edu>
2017-11-15 11:52:33 -0800

    Some camera permissions and other stuff.

--- SeeFood2/app/src/main/java/com/ash/fish/seefood/MainActivity.java
+++ SeeFood2/app/src/main/java/com/ash/fish/seefood/MainActivity.java
@@ -14,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
         myButton  = (Button)findViewById(R.id.colorBtn);
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
