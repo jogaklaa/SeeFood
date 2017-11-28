5e96454bbe0401d760eff49aa14f94ad5064c951
Hannah Muetzel <hannahmuetzel@gmail.com>
2017-11-28 16:38:54 -0500

    Almost have a gallery!!
    Thanks, Internet!

---
+++ SeaFood/app/src/main/java/com/ash/fish/seafood/ProductListActivity.java
@@ -0,0 +1,24 @@
+package com.ash.fish.seafood;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Hannah Muetzel on 11/28/2017.
+ */
+
+public class ProductListActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.product_list);
+
+
+
+
+    }
+
+
+}
