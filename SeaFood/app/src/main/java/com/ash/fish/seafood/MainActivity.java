2d8b9613edaed80afb9ac50bed613b963d09bb2e
Hannah Muetzel <hannahmuetzel@gmail.com>
2017-11-29 15:37:15 -0500

    bye bye product files
    you will not be missed

--- SeaFood/app/src/main/java/com/ash/fish/seafood/MainActivity.java
+++ SeaFood/app/src/main/java/com/ash/fish/seafood/MainActivity.java
@@ -24,12 +24,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import com.android.volley.toolbox.StringRequest;
-import org.json.JSONArray;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE =200;
@@ -39,13 +33,6 @@ public class MainActivity extends AppCompatActivity {
 
     //this is the JSON Data URL
     //make sure you are using the correct ip else it will not work
-    private static final String URL_PRODUCTS = "http://34.234.229.114:8000/fetch";
-
-    //a list to store all the products
-    List<Product> productList;
-
-    //the recyclerview
-    RecyclerView recyclerView;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -54,19 +41,6 @@ public class MainActivity extends AppCompatActivity {
 
         //initializing views
         imageView = (ImageView) findViewById(R.id.imageView);
-
-        //getting the recyclerview from xml
-        recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        //initializing the productlist
-        productList = new ArrayList<>();
-
-        //this method will fetch and parse json
-        //to display it in recyclerview
-        loadProducts();
-
 
         findViewById(R.id.buttonGallery).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -206,54 +180,5 @@ public class MainActivity extends AppCompatActivity {
 
         //adding the request to volley
         Volley.newRequestQueue(this).add(volleyMultipartRequest);
-    }
-    private void loadProducts() {
-
-        /*
-        * Creating a String Request
-        * The request type is GET defined by first parameter
-        * The URL is defined in the second parameter
-        * Then we have a Response Listener and a Error Listener
-        * In response listener we will get the JSON response as a String
-        * */
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL_PRODUCTS,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            //converting the string to json array object
-                            JSONArray array = new JSONArray(response);
-
-                            //traversing through all the object
-                            for (int i = 0; i < array.length(); i++) {
-
-                                //getting product object from json array
-                                JSONObject product = array.getJSONObject(i);
-
-                                //adding the product to product list
-                                productList.add(new Product(
-                                        product.getString("imageUrl"),
-                                        product.getDouble("negative"),
-                                        product.getDouble("positive")
-                                ));
-                            }
-
-                            //creating adapter object and setting it to recyclerview
-                            ProductsAdapter adapter = new ProductsAdapter(MainActivity.this, productList);
-                            recyclerView.setAdapter(adapter);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                    }
-                });
-
-        //adding our stringrequest to queue
-        Volley.newRequestQueue(this).add(stringRequest);
     }
 }
